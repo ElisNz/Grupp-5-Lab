02f52d5823a559b679d365f11733e21946de1db7
David Johnson <73168048+dejv0ld@users.noreply.github.com>
2023-05-17 10:58:41 +0200

    Update repository.js

--- server/repositories/repository.js
+++ server/repositories/repository.js
@@ -1,36 +1,7 @@
 const customerModel = require("../models/customerModel");
 const db_context = require("../db_context");
 
-// async function getAllCustomers() {
 
-//     let customers = [];
-
-//     let data = await db_context.selectAllCustomers()
-
-//     data.forEach(customer => {
-//         customers.push(new customerModel(customer.first_name, customer.email))
-//     });
-
-//     return customers;
-// };
-
-async function getAllCustomers() {
-
-    let customers = [];
-
-    let data = await db_context.selectAllCustomers()
-
-    data.forEach(customer => {
-        customers.push(new customerModel(customer.first_name, customer.email))
-    });
-
-    return customers;
-};
-
-async function editCustomer(id, email) {
-
-    db_context.updateCustomerEmailById(id, email);
-};
 
 module.exports = {
     getAllCustomers,
