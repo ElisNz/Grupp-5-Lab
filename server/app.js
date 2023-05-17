e07da6856f6fed26588f7d4419eb66b486a9b7a7
David Johnson <73168048+dejv0ld@users.noreply.github.com>
2023-05-17 10:55:04 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,10 +8,6 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 
-const customerRouter = require('./routes/customer');
-app.use('/customer', customerRouter);
 
-const cityRouter = require('./routes/city');
-app.use('/city', cityRouter);
 
 app.listen(3000);
