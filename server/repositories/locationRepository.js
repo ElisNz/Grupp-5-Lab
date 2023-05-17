b38683eff60ce0b4c2747c0f0af3c8387ee501c7
David Johnson <73168048+dejv0ld@users.noreply.github.com>
2023-05-17 10:59:24 +0200

    Update locationRepository.js

--- server/repositories/locationRepository.js
+++ server/repositories/locationRepository.js
@@ -1,35 +1,9 @@
 const cityModel = require("../models/cityModel");
 const db_context = require("../db_context");
 
-async function getAllCities() {
 
-    let cities = [];
 
-    let data = await db_context.selectAllCities()
 
-    data.forEach(city => {
-        cities.push(new cityModel(city.city))
-    });
-
-    return cities;
-};
-
-async function addCity(city, country_id) {
-
-    let result = await db_context.insertCity(city, country_id)
-
-    return result;
-};
-
-async function removeCity(city, country_id) {
-
-    let result = await db_context.deleteCity(city, country_id)
-
-    return result;
-};
 
 module.exports = {
-    getAllCities,
-    addCity,
-    removeCity
 }
