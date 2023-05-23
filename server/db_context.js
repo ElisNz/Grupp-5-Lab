3da38809299c09961f7196a2c98937b84dbb9757
Fetiamohammed <fetia-awol@hotmail.com>
2023-05-23 09:21:04 +0200

    new update on model.js

--- server/db_context.js
+++ server/db_context.js
@@ -1,29 +1,12 @@
-const mongoose = require('mongoose');
+const uri = 'mongodb+srv://enz:MV6qCi73W7sQ7sTG@cluster0.ohmduf8.mongodb.net/?retryWrites=true&w=majority';
 
-const uri ='mongodb+srv://enz:MV6qCi73W7sQ7sTG@cluster0.ohmduf8.mongodb.net/mydatabase?retryWrites=true&w=majority';
+async function updateCustomerEmailById(id, email) {}
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to the MongoDB database');
-  })
-  .catch((error) => {
-    console.error('Error connecting to the MongoDB database:', error);
-  });
+async function insertLocation() {}
 
-async function updateCustomerEmailById(id, email) {
-  // Implement your update logic here
-}
+async function deleteLocation() {}
 
-async function insertLocation() {
-  // Implement your insert logic here
-}
-
-async function deleteLocation() {
-  // Implement your delete logic here
-}
 
 module.exports = {
-  updateCustomerEmailById,
-  insertLocation,
-  deleteLocation
-};
+  uri
+}
