ec95cccd740b11ea87e18526e9ad6ef71c8e9074
Fetiamohammed <fetia-awol@hotmail.com>
2023-05-23 14:59:24 +0200

    small editing

--- server/db_context.js
+++ server/db_context.js
@@ -1,5 +1,4 @@
-const uri =
-    'mongodb+srv://enz:MV6qCi73W7sQ7sTG@cluster0.ohmduf8.mongodb.net/?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://enz:MV6qCi73W7sQ7sTG@cluster0.ohmduf8.mongodb.net/?retryWrites=true&w=majority';
 
 async function updateCustomerEmailById(id, email) {}
 
@@ -7,6 +6,7 @@ async function insertLocation() {}
 
 async function deleteLocation() {}
 
+
 module.exports = {
-    uri
+  uri
 }
